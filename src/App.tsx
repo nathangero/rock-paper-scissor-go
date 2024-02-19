93f425ef75ed1d7968bde7ff9136b7878737753c
Nathan Geronimo <nathangero@yahoo.com>
2024-02-19 14:47:46 -0800

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import { Outlet } from "react-router-dom"
 import Navbar from "./components/Navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   useEffect(() => {
   })
