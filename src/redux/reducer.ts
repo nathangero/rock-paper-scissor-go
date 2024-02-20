8c23d9aef193c5087264e92b3868decd7bc13bd1
Nathan Geronimo <nathangero@yahoo.com>
2024-02-20 14:48:45 -0800

    adjust some styling

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -11,8 +11,6 @@ const initialState = {
 export const reducer = (state = initialState, action: any) => {
   switch (action.type) {
     case USER_ACTIONS.LOGIN:
-      console.log("@user login");
-      console.log("action.user:", action.user);
       return {
         ...state,
         user: action.user
