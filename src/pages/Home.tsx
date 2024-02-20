a50c7af9e4f9d1ba86319fb0a4be3f7583d13b1d
Nathan Geronimo <nathangero@yahoo.com>
2024-02-19 16:15:44 -0800

    implement react redux

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
+import { useAppSelector } from "../redux/hooks"
+
 export default function Home() {
+
+  const user = useAppSelector(state => state.user);
 
   return (
     <>
       <h1>Rock Paper Scissor, Go!</h1>
 
-      <h4>Play against others in Rock, Paper, Scissors and move up in rank!</h4>
+      {user ?
+        <h4>Welcome back, {user?.username}!</h4> :
+        <h4>Play against others in Rock, Paper, Scissors and move up in rank!</h4>
+      }
 
       <button>Play For Fun</button>
       <button>Play For Rank</button>
